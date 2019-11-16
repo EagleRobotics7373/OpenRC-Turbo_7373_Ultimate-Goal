32641ca604df68f8d370279118e7767e632aa628
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2019-11-16 00:00:21 -0500

    TeleOp comments for easier readability and future editing (#4)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpFOD.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpFOD.kt
@@ -6,10 +6,18 @@ import org.firstinspires.ftc.teamcode.library.functions.rangeBuffer
 import org.firstinspires.ftc.teamcode.library.functions.reverseIf
 import org.firstinspires.ftc.teamcode.library.robot.robotcore.IMUController
 
+/**
+ * Driver-operated opmode for field-oriented drive, as an extension of base TeleOp functions
+ */
 @TeleOp(name="TeleOp FOD", group="basic")
 class TeleOpFOD : TeleOpRD() {
+    // when this is true, field-oriented drive is enabled
     var fod = false
+
+    // baseline value for IMU heading value
     var zeroAngle = 0.0
+
+    // imu controller; uses lateinit keyword to signify instantiation after opmode init
     lateinit var imuController:IMUController
 
     override fun init() {
@@ -20,28 +28,43 @@ class TeleOpFOD : TeleOpRD() {
 
     }
 
+    /**
+     * override function that runs instead of TeleOpRD.controlDrivetrain()
+     */
     override fun controlDrivetrain() {
+        // enable field-oriented drive if 'x' button is pressed
         if (gamepad1.x) {
             fod = true
         }
+        // disable field-oriented drive if 'a' or 'b' buttons are pressed
         else if (gamepad1.a or gamepad1.b) fod = false
 
 
         if (fod)
         {
+            // reset IMU baseline if left stick button is pressed
             if (gamepad1.left_stick_button) zeroAngle = imuController.getHeading()
+
+            // Set x, y, and z inputs:
+            //    Use Double.rangeBuffer to set motor power dead-band
+            //    Use Double.times() for variable speed. (This functional method works the same as doing x * y in Java)
             val x = gamepad1.left_stick_x.toDouble().rangeBuffer(-0.15, 0.15, 0.0).times(0.33*speed)
             val y = -gamepad1.left_stick_y.toDouble().rangeBuffer(-0.15, 0.15, 0.0).times(0.33*speed)
             val z = gamepad1.right_stick_x.toDouble().rangeBuffer(-0.15, 0.15, 0.0).times(0.33*speed)
+
+            // Set drivetrain values, including an offset angle for FOD
             robot.holonomic.runWithoutEncoderVectored(x, y, z, zeroAngle - imuController.getHeading())
-        } else super.controlDrivetrain()
+        }
+        // if FOD is not enabled, call superclass relative driving method instead
+        else super.controlDrivetrain()
+
+        // speed control using button watchers
         if (watch_gamepad1_dpadDown.call() and (speed > 1)) speed--
         if (watch_gamepad1_dpadUp.call() and (speed < 3)) speed++
+
+        // basic telemetry
         telemetry.addData("fod", fod)
         telemetry.addData("zero angle", zeroAngle)
-    }
-
-    private fun controlMusic() {
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpRD.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpRD.kt
@@ -10,8 +10,10 @@ import kotlin.math.absoluteValue
 
 @TeleOp(name="TeleOp RD", group="basic")
 open class TeleOpRD : OpMode() {
+    // robot hardware declaration; assignment occures during init()
     lateinit var robot : BasicRobot
 
+    // gamepad toggle button watchers, instantiated after opmode init
     protected lateinit var watch_gamepad1_buttonY : ToggleButtonWatcher
     protected lateinit var watch_gamepad1_dpadDown : ToggleButtonWatcher
     protected lateinit var watch_gamepad1_dpadUp : ToggleButtonWatcher
@@ -26,19 +28,21 @@ open class TeleOpRD : OpMode() {
     var speed = 3
 
     override fun init() {
+        // instantiate robot variables
         robot = BasicRobot(hardwareMap)
+
+        // Instantiate toggle button watchers. Each statement below is calling a constructor with a single
+        //   parameter, in this case being a function for calling the gamepad button. This does not set the
+        //   current gamepad state only in the ToggleButtonWatcher; rather, it sets the ability for the
+        //   ToggleButtonWatcher to call the gamepad values.
         watch_gamepad1_buttonY = ToggleButtonWatcher {gamepad1.y}
         watch_gamepad1_dpadDown = ToggleButtonWatcher {gamepad1.dpad_down}
         watch_gamepad1_dpadUp = ToggleButtonWatcher {gamepad1.dpad_up}
         watch_gamepad2_rightStickButton = ToggleButtonWatcher { gamepad2.right_stick_button }
     }
-// speed control, down = 50%, down = 25% (done with 1/3s)
-    // triggers for wheels, right = block goes up (done)
-    //right bumper close
-    //left bumper open
-    //right stick push for barely out
 
     override fun loop() {
+        // call methods for each individual system for streamlined navigation
         controlDrivetrain()
         controlFoundationGrabbers()
         controlIntakeMechanism()
@@ -47,23 +51,37 @@ open class TeleOpRD : OpMode() {
     }
 
     override fun stop() {
+        // overrides OpMode.stop() to ensure hardware components and music player stop
         super.stop()
         musicPlayer.stop()
     }
 
     protected open fun controlDrivetrain() {
+        // Set x, y, and z inputs:
+        //    Use Double.rangeBuffer to set motor power dead-band
+        //    Use Double.times() for variable speed. (This functional method works the same as doing x * y in Java)
+        //    Use Double.reverseIf() on x and y to easily reverse input direction
         val x = gamepad1.left_stick_x .toDouble().rangeBuffer(-0.1, 0.1, 0.0).times(0.33*speed).reverseIf(reverse)
         val y = -gamepad1.left_stick_y.toDouble().rangeBuffer(-0.1, 0.1, 0.0).times(0.33*speed).reverseIf(reverse)
         val z = gamepad1.right_stick_x.toDouble().rangeBuffer(-0.1, 0.1, 0.0).times(0.33*speed)
 
-
-//        robot.holonomic.runWithoutEncoderVectored(x, y, z, 0.0)
+        // set gamepad inputs to robot output
         robot.holonomic.runWithoutEncoder(x, y, z)
+
+        // read 'a' and 'b' buttons to reverse robot orientation
         if (gamepad1.a) reverse = false
         else if (gamepad1.b) reverse = true
 
+        // reverse robot orientation using toggle feature in addition to static buttons
+        // if y button is pressed, make reverse variable opposite of what it is now
+        //   then wait until y is released before letting it change the variable again
         if (watch_gamepad1_buttonY.call()) reverse = !reverse
-        // do y for toggle
+
+        // increase or decrease robot speed variable using toggle system
+        // speed variable should be between 1 and 3
+        //    3 = max power (0.33 * 3 * input)
+        //    2 = mid power (0.33 * 2 * input)
+        //    1 = low power (0.33 * 1 * input)
         if (watch_gamepad1_dpadDown.call() and (speed > 1)) speed--
         if (watch_gamepad1_dpadUp.call() and (speed < 3)) speed++
 
@@ -71,10 +89,13 @@ open class TeleOpRD : OpMode() {
     }
 
     private fun controlFoundationGrabbers() {
+        // raise or lower foundation grabbers using gamepad2 dpad
         if (gamepad2.dpad_down) robot.foundationGrabbers.lock()
         else if (gamepad2.dpad_up) robot.foundationGrabbers.unlock()
     }
 
+    // set PID control coefficients for auto-intake raise in controlIntakeMechanism()
+    // this should eventually be moved
     private var intakeP = 4.0
     private var intakeI = 0.000
     private var intakeD = 0.20
@@ -83,10 +104,15 @@ open class TeleOpRD : OpMode() {
     private var d = 0.0
     private var lastError = 0.0
     private var errorSum = 0.0
+
     private fun controlIntakeMechanism() {
         // control intake arm up/down
+        // This variable is being set by a Kotlin expression -
+        //   meaning the variable will be set to last value in an if/else/when branch
         val input =
+                // if left stick button is pressed, set input to auto-raise value
                 if (gamepad2.left_stick_button) {
+
                     //use pid loop
                     val error = voltageTarget-robot.intakePivotPotentiometer.voltage
                     if (lastRuntime==-1.0) lastRuntime = runtime
@@ -96,24 +122,37 @@ open class TeleOpRD : OpMode() {
                     lastRuntime = runtime
                     lastError = error
 
+                    // this is the last expression in this if branch, so this would be assigned to 'input':
                     -(intakeP * error + intakeI * errorSum + intakeD * d)
                 }
-                else gamepad2.left_stick_y.toDouble()
+                else  {
+                    // this is the last statement in this else branch, so this would be assigned to 'input':
+                    gamepad2.left_stick_y.toDouble()
+                }
 
+        // set intake pivot motor power using Kotlin expression assignment
         robot.intakePivotMotor.power =
                 when {
+                    // if pressing right gamepad bumper, use constant power of 0.02
                     (gamepad2.right_bumper)-> if (input > 0.0) input+0.02 else 0.02 - -input*0.04
+
+                    // if input is greater than 0, give full increase starting at constant power of 0.12
                     input > 0.0 -> input+0.12
-                    else -> 0.12 - -input*0.14
+
+                    // otherwise, give constant power at 0.12 and diminished decrease (0.12 + input * 0.14)
+                    else -> 0.12 + input*0.14
                 }/*.rangeBuffer(-0.10, 0.10, 0.0)*/
 
-        // control block grabbing
+        // if gamepad2 right stick y is being used outside of dead-band, give it priority control over intake wheels
         if (gamepad2.right_stick_y.toDouble().absoluteValue > 0.10) robot.intakeBlockManipulator.power = -gamepad2.right_stick_y.toDouble()
+        // if gamepad1 right trigger is pressed, let it drive wheels to intake blocks
         else if (gamepad1.right_trigger > 0.03) robot.intakeBlockManipulator.power = gamepad1.right_trigger.toDouble()
+        // if gamepad1 left trigger is pressed, let it drive wheels to release blocks
         else if (gamepad1.left_trigger > 0.03) robot.intakeBlockManipulator.power = -gamepad1.left_trigger.toDouble()
+        // otherwise do not drive intake wheels
         else robot.intakeBlockManipulator.power = 0.0
 
-        // control grabbing servo
+        // control grabbing servo using basic gamepad input
         if (gamepad2.x or gamepad1.right_bumper) robot.intakeBlockGrabber.hold()
         else if (gamepad2.y or gamepad1.left_bumper) robot.intakeBlockGrabber.release()
         else if (gamepad2.a or gamepad1.right_stick_button) robot.intakeBlockGrabber.mid()
@@ -121,6 +160,7 @@ open class TeleOpRD : OpMode() {
     }
 
     private fun controlMusic() {
+        // if gamepad2 right stick button is toggled, toggle music
         if (watch_gamepad2_rightStickButton.call()) {
             if (musicPlayer.isPlaying()) musicPlayer.pause()
             else musicPlayer.play()
@@ -141,12 +181,17 @@ open class TeleOpRD : OpMode() {
 //        telemetry.addData("DS distance", robot.intakeBlockDSensor.getDistance(DistanceUnit.MM))
         telemetry.update()
     }
+
+    // this function is not used
     fun setCoeffs(p: Double, i: Double, d: Double) {
         intakeP = p
         intakeI = i
         intakeD = d
     }
 
+    // functionality is explained throughout opmode; allows for encapsulation of button presses
+    //   that toggle changes in variables
+    // constructor parameter is a function (no parameters, returns Boolean)
     protected class ToggleButtonWatcher(private val getMethod: ()->Boolean) {
         private var lastState : Boolean = getMethod()
         fun call(): Boolean {
